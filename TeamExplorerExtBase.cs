b18794e4362d0f17577e0e2c217a64121596e7ba
ramsankar184 <64629462+ramsankar184@users.noreply.github.com>
2020-05-01 11:33:32 +0530

    Add files via upload

---
+++ TeamExplorerExtBase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.TeamFoundation.Controls;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace TeamExplorerExtension
+{
+    [TeamExplorerNavigationItem("A60CD381 - 2D27 - 49E5 - B3E8 - EFE13BCF5435",1800)]
+    public class TeamExplorerExtBase : ITeamExplorerNavigationItem2
+    {
+        public bool IsEnabled => true;
+
+		public int ArgbColor => 0;
+
+        public object Icon => null;
+
+        public string Text => "ViewCS";
+
+        public Image Image => null;
+
+        public bool IsVisible => true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+		}
+
+		public void Execute()
+		{
+			Process.Start("notepad.exe");
+		}
+
+        public void Invalidate()
+        {
+            
+        }
+    }
+}
